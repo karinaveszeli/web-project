c05f721b375cefa817a16eca860ac2ea49917c55
Veszeli Karina <h166120@stud.u-szeged.hu>
2025-02-03 22:04:59 +0100

    Add and format text content

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,34 @@ const App = () => {
   return (
     <div>
        <Header />
-      <main className="p-8">
-        <p>Welcome to my blog!</p>
-       
+       <main className="p-8 w-[56.666%] mx-auto">
+        <h1 className="text-2xl font-bold text-center mb-4">The purpose of our lives is to be happy.</h1>
 
+        <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] ">
+          Quickly monetize synergistic portals via efficient e-commerce. Phosfluorescently reinvent frictionless
+          intellectual capital vis-a-vis an expanded array of niches. Efficiently provide access to interactive scenarios
+          vis-a-vis bleeding-edge value. Energistically maximize premium meta-services and multifunctional collaboration
+          and idea-sharing. Dramatically unleash front-end value after out-of-the-box opportunities. Energistically streamline magnetic
+          action items after global markets. Collaboratively scale backward-compatible leadership skills and 24/7 best
+          practices. Objectively exploit emerging methodologies vis-a-vis functionalized ideas.
+          Energistically reconceptualize out-of-the-box innovation vis-a-vis cooperative methods of empowerment. Uniquely
+          foster revolutionary value whereas accurate communities. 
+        </p>
+
+      
+          <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
+          Conveniently engineer robust experiences after performance-based initiatives.Credibly communicate strategic technology whereas turnkey functionalities. Professionally re-engineer magnetic
+          markets for highly efficient functionalities. Efficiently fabricate user friendly opportunities for
+          backward-compatible experiences. Competently scale team building relationships through real-time solutions.
+        </p>
+
+        <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
+          Interactively productivate cross-platform vortals through tactical results. Interactively underwhelm efficient
+          alignments with enabled materials. Rapidiously leverage existing superior technology via fully researched
+          e-commerce.
+          Efficiently whiteboard principle-centered portals vis-a-vis magnetic solutions. Conveniently network ethical
+          e-markets with emerging web services.
+        </p>
       </main>
       
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div className="h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh] mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
+    <div className="min-h-screen mb-4 bg-cover bg-center flex 
+    items-center w-full overflow-hidden "
     style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
+      
     <NavBar/>
 
     </div>

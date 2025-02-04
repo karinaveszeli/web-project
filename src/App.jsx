0134cd26a8b620ee9201ce32448ead02d8735794
Veszeli Karina <h166120@stud.u-szeged.hu>
2025-02-04 14:29:20 +0100

    Add two nature images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import image1 from "./assets/images/img1.png";
+import image2 from "./assets/images/img2.png";
+
 
 const App = () => {
   return (
     <div>
-       <Header />
-       <main className="p-8 w-[56.666%] mx-auto justify-start">
+      <Header />
+      <main className="p-8 w-[56.666%] mx-auto justify-start">
         <h1 className="text-2xl font-bold  mb-4">The purpose of our lives is to be happy.</h1>
 
         <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins]  ">
@@ -17,11 +20,11 @@ const App = () => {
           action items after global markets. Collaboratively scale backward-compatible leadership skills and 24/7 best
           practices. Objectively exploit emerging methodologies vis-a-vis functionalized ideas.
           Energistically reconceptualize out-of-the-box innovation vis-a-vis cooperative methods of empowerment. Uniquely
-          foster revolutionary value whereas accurate communities. 
+          foster revolutionary value whereas accurate communities.
         </p>
 
-      
-          <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
+
+        <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
           Conveniently engineer robust experiences after performance-based initiatives.Credibly communicate strategic technology whereas turnkey functionalities. Professionally re-engineer magnetic
           markets for highly efficient functionalities. Efficiently fabricate user friendly opportunities for
           backward-compatible experiences. Competently scale team building relationships through real-time solutions.
@@ -36,39 +39,45 @@ const App = () => {
         </p>
 
         <h1 className="text-2xl font-bold  mb-4">
-        Many of life’s failures are people who did not<br /> 
-        realize how close they were to success when <br />
-        they gave up.</h1>
+          Many of life’s failures are people who did not<br />
+          realize how close they were to success when <br />
+          they gave up.</h1>
 
         <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
-        Quickly monetize synergistic portals via efficient e-commerce. 
-        Phosfluorescently reinvent frictionless intellectual capital vis-a-vis an expanded array of niches.
-        Efficiently provide access to interactive scenarios vis-a-vis bleeding-edge value. Energistically maximize
-        premium meta-services and multifunctional collaboration and idea-sharing. Dramatically unleash front-end 
-        value after out-of-the-box opportunities. Energistically streamline magnetic action items after global markets.
-        Collaboratively scale backward-compatible leadership skills and 24/7 best practices. Objectively exploit 
-        emerging methodologies vis-a-vis functionalized ideas. Energistically reconceptualize out-of-the-box 
-        innovation vis-a-vis cooperative methods of empowerment. Uniquely foster revolutionary value whereas 
-        accurate communities. 
+          Quickly monetize synergistic portals via efficient e-commerce.
+          Phosfluorescently reinvent frictionless intellectual capital vis-a-vis an expanded array of niches.
+          Efficiently provide access to interactive scenarios vis-a-vis bleeding-edge value. Energistically maximize
+          premium meta-services and multifunctional collaboration and idea-sharing. Dramatically unleash front-end
+          value after out-of-the-box opportunities. Energistically streamline magnetic action items after global markets.
+          Collaboratively scale backward-compatible leadership skills and 24/7 best practices. Objectively exploit
+          emerging methodologies vis-a-vis functionalized ideas. Energistically reconceptualize out-of-the-box
+          innovation vis-a-vis cooperative methods of empowerment. Uniquely foster revolutionary value whereas
+          accurate communities.
         </p>
 
         <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
-        Conveniently engineer robust experiences after performance based initiatives. 
-        Credibly communicate strategic technology whereas turnkey functionalities. Professionally re-engineer 
-        magnetic markets for highly efficient functionalities. Efficiently fabricate user friendly opportunities 
-        for backward-compatible experiences.Competently scale team building relationships through real-time solutions.
+          Conveniently engineer robust experiences after performance based initiatives.
+          Credibly communicate strategic technology whereas turnkey functionalities. Professionally re-engineer
+          magnetic markets for highly efficient functionalities. Efficiently fabricate user friendly opportunities
+          for backward-compatible experiences.Competently scale team building relationships through real-time solutions.
         </p>
-      
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-12">
+          <img src={image1} alt="Nature 1" className="rounded-lg shadow-md object-cover w-full h-auto" />
+          <img src={image2} alt="Nature 2" className="rounded-lg shadow-md object-cover w-full h-auto" />
+
+        </div>
+
         <p className="text-[#6C757D] text-[14px] leading-[25px] font-normal font-[Poppins] mt-4">
-      Quickly monetize synergistic portals via efficient e-commerce. Phosfluorescently reinvent frictionless
-      intellectual capital vis-a-vis an expanded array of niches. Efficiently provide access to interactive 
-      scenarios vis-a-vis bleeding-edge value. Energistically maximize premium meta-services and multifunctional 
-      collaboration and idea-sharing. Dramatically unleash front-end value after out-of-the-box opportunities. 
-      Energistically streamline magnetic action items after global markets. Collaboratively scale backward-compatible 
-      leadership skills and 24/7 best practices. Objectively exploit emerging methodologies vis-a-vis functionalized 
-      ideas.Energistically reconceptualize out-of-the-box innovation vis-a-vis cooperative methods of empowerment.
-      </p>
-      
+          Quickly monetize synergistic portals via efficient e-commerce. Phosfluorescently reinvent frictionless
+          intellectual capital vis-a-vis an expanded array of niches. Efficiently provide access to interactive
+          scenarios vis-a-vis bleeding-edge value. Energistically maximize premium meta-services and multifunctional
+          collaboration and idea-sharing. Dramatically unleash front-end value after out-of-the-box opportunities.
+          Energistically streamline magnetic action items after global markets. Collaboratively scale backward-compatible
+          leadership skills and 24/7 best practices. Objectively exploit emerging methodologies vis-a-vis functionalized
+          ideas.Energistically reconceptualize out-of-the-box innovation vis-a-vis cooperative methods of empowerment.
+        </p>
+
       </main>
       <Footer />
     </div>
